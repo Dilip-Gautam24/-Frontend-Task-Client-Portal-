0c48e41636e652b84ba3bf14e7703fd55601ab97
Dilip-Gautam24 <65763050+Dilip-Gautam24@users.noreply.github.com>
2022-12-25 20:19:30 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 import { useState, useEffect, useMemo } from "react";
 
